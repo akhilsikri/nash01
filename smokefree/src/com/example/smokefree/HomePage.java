0c6b9f37b0ac9fd3ccf512d0e002d138a1ecb26a
Akhil Sikri <asikri@kiwiup.com>
2013-03-18 23:55:02 +0530

    main page view

--- smokefree/src/com/example/smokefree/HomePage.java
+++ smokefree/src/com/example/smokefree/HomePage.java
@@ -2,7 +2,9 @@ package com.example.smokefree;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class HomePage extends Activity {
 
@@ -17,6 +19,11 @@ public class HomePage extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home_page, menu);
 		return true;
+	}
+	
+	public void getStatistics(View view) {
+		Intent statisticsIntent = new Intent();
+		// start activity of new page
 	}
 
 }
